774f329bf7b8fb59b60138d887ea72fcf7d52150
Prachi Phadke <prachi.phadke@microchip.com>
2021-07-08 18:42:46 +0530

    [PMSM_FOC_PLL_ESTIMATOR_SAM_E54] Regenerated demo for pmsm_foc_pll_estimator_sam_e54_mchv3_sam_e54_pim
    MH3-55105
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/pmsm_foc_pll_estimator_sam_e54/firmware/src/config/mchv3_sam_e54_pim/peripheral/eic/plib_eic.c
+++ apps/pmsm_foc_pll_estimator_sam_e54/firmware/src/config/mchv3_sam_e54_pim/peripheral/eic/plib_eic.c
@@ -54,6 +54,7 @@
 */
 
 #include "plib_eic.h"
+#include "interrupts.h"
 
 // *****************************************************************************
 // *****************************************************************************
@@ -67,7 +68,7 @@ void EIC_Initialize (void)
 {
     /* Reset all registers in the EIC module to their initial state and
 	   EIC will be disabled. */
-    EIC_REGS->EIC_CTRLA |= EIC_CTRLA_SWRST_Msk;
+    EIC_REGS->EIC_CTRLA |= (uint8_t)EIC_CTRLA_SWRST_Msk;
 
     while((EIC_REGS->EIC_SYNCBUSY & EIC_SYNCBUSY_SWRST_Msk) == EIC_SYNCBUSY_SWRST_Msk)
     {
@@ -101,16 +102,16 @@ void EIC_Initialize (void)
 
 
     /* Debouncer enable */
-    EIC_REGS->EIC_DEBOUNCEN = 0x4;
+    EIC_REGS->EIC_DEBOUNCEN = 0x4U;
 
     /* Event Control Output enable */
-    EIC_REGS->EIC_EVCTRL = 0x4;
+    EIC_REGS->EIC_EVCTRL = 0x4U;
 
     /* Debouncer Setting */
-    EIC_REGS->EIC_DPRESCALER = EIC_DPRESCALER_PRESCALER0(0) | EIC_DPRESCALER_PRESCALER1(0) ;
+    EIC_REGS->EIC_DPRESCALER = EIC_DPRESCALER_PRESCALER0(0UL) | EIC_DPRESCALER_PRESCALER1(0UL) ;
 
     /* Enable the EIC */
-    EIC_REGS->EIC_CTRLA |= EIC_CTRLA_ENABLE_Msk;
+    EIC_REGS->EIC_CTRLA |= (uint8_t)EIC_CTRLA_ENABLE_Msk;
 
     while((EIC_REGS->EIC_SYNCBUSY & EIC_SYNCBUSY_ENABLE_Msk) == EIC_SYNCBUSY_ENABLE_Msk)
     {
